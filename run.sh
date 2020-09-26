04df0cc01791de7ef807ecd6f96a072ecd8fdd97
Your Name <you@example.com>
2020-09-26 14:08:19 +0900

    Add files.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PROJECT=$1
 shift
 MODE=$1
 
-echo "Running mode is [ $MODE ]."
+echo "Running mode is [ $MODE ]"
 
 cd /app
 
@@ -13,18 +13,27 @@ if [ -e $PROJECT ]; then
   git submodule update
 fi
 
-if [ $MODE = "build" ]; then
-  path=$(pwd)
-  if [ ! -e public ]; then
-    mkdir public
-  fi
-  cd /temp
-  ln -s $path/.* ./ &>/dev/null
-  ln -s $path/* ./ &>/dev/null
-  ln -s /node/node_modules/ ./
-  hugo
-elif [ $MODE = "bash" ]; then
-  bash
-elif [ $MODE = "server" ]; then
-  hugo server --bind 0.0.0.0
-fi
+case $MODE in
+  "build" )
+      path=$(pwd)
+      if [ ! -e public ]; then
+        mkdir public
+      fi
+      cd /temp
+      ln -s $path/.* ./ &>/dev/null
+      ln -s $path/* ./ &>/dev/null
+      ln -s /node/node_modules/ ./
+      hugo;;
+  "server" )
+      hugo server --bind 0.0.0.0;;
+  "bash" )
+      bash;;
+  "deploy" )
+      until [ -e /share/done-build ]; do sleep 1; done;
+      echo "OK!!";;
+  "pdf" )
+      until [ $(curl -LI http://docsy:1313/docs/contribution-guidelines/ -o /dev/null -s -w '%{http_code}\n') -eq 200 ]; do sleep 1; done;
+      /usr/local/bin/entrypoint --include-background --url http://docsy:1313/docs/contribution-guidelines/ --pdf out.pdf;;
+  *)
+      echo "Unknown mode."
+esac
